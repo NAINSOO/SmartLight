f9311dbaefc82b841e9a02b408e5a7c1b61e4b8d
Nainsoo <i12force@naver.com>
2022-11-27 17:08:25 +0900

    bluetooth function is added

--- user/main.c
+++ user/main.c
@@ -9,6 +9,7 @@
 #include <Touch.h>
 #include <stdio.h>
 #include <stdlib.h>
+
 /* function prototype */
 void RCC_Configure(void);
 void GPIO_Configure(void);
@@ -18,12 +19,29 @@ void NVIC_Configure(void);
 //---------------------------------------------------------------------------------------------------
 
 uint16_t jodo;
-int j;
-int t = 1;
+void USART1_Init(void);
 
-void delay(int i){
-	int j;
-	for(j=0; j<=i * 100000; j++);
+void EXTI15_10_IRQHandler(void);
+
+void Delay(void);
+
+
+void send_com(char buf[]) {
+char *s = buf;
+while (*s) {
+while (USART_GetFlagStatus(USART1, USART_FLAG_TXE) == RESET)
+;
+USART_SendData(USART1, *s++);
+}
+}
+
+void send_phone(char buf[]) {
+char *s = buf;
+while (*s) {
+while (USART_GetFlagStatus(USART2, USART_FLAG_TXE) == RESET)
+;
+USART_SendData(USART2, *s++);
+}
 }
 
 void set_ENABLE(void) {
@@ -87,6 +105,109 @@ void ADC1_2_IRQHandler(void) {
 	//	LCD_DrawRectangle(pix_x, pix_y, pix_x+10, pix_y+10);
 }
 
+void USART1_Init(void)
+{
+  USART_InitTypeDef usart1_init_struct;
+  GPIO_InitTypeDef gpioa_init_struct;
+  NVIC_InitTypeDef NVIC_InitStructure;
+
+  RCC_APB2PeriphClockCmd(RCC_APB2Periph_USART1 | RCC_APB2Periph_AFIO |
+ RCC_APB2Periph_GPIOA, ENABLE);
+
+  gpioa_init_struct.GPIO_Pin = GPIO_Pin_9;
+  gpioa_init_struct.GPIO_Speed = GPIO_Speed_50MHz;
+  gpioa_init_struct.GPIO_Mode = GPIO_Mode_AF_PP;
+  GPIO_Init(GPIOA, &gpioa_init_struct);
+  gpioa_init_struct.GPIO_Pin = GPIO_Pin_10;
+  gpioa_init_struct.GPIO_Speed = GPIO_Speed_50MHz;
+  gpioa_init_struct.GPIO_Mode = GPIO_Mode_IN_FLOATING;
+  GPIO_Init(GPIOA, &gpioa_init_struct);
+
+  usart1_init_struct.USART_BaudRate = 9600;
+  usart1_init_struct.USART_WordLength = USART_WordLength_8b;
+  usart1_init_struct.USART_StopBits = USART_StopBits_1;
+  usart1_init_struct.USART_Parity = USART_Parity_No;
+  usart1_init_struct.USART_Mode = USART_Mode_Rx | USART_Mode_Tx;
+  usart1_init_struct.USART_HardwareFlowControl = USART_HardwareFlowControl_None;
+  USART_Init(USART1, &usart1_init_struct);
+  USART_Cmd(USART1, ENABLE);
+  USART_ITConfig(USART1, USART_IT_RXNE, ENABLE);
+
+  NVIC_InitStructure.NVIC_IRQChannel = USART1_IRQn;
+  NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 0x01;
+  NVIC_InitStructure.NVIC_IRQChannelSubPriority = 0x01;
+  NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
+  NVIC_Init(&NVIC_InitStructure);
+}
+void USART2_Init(void)
+{
+  USART_InitTypeDef usart2_init_struct;
+  GPIO_InitTypeDef gpioa_init_struct;
+  NVIC_InitTypeDef NVIC_InitStructure;
+
+  RCC_APB1PeriphClockCmd(RCC_APB1Periph_USART2|  RCC_APB2Periph_AFIO |
+ RCC_APB2Periph_GPIOA, ENABLE);
+
+  // tx, rx ¼³Á¤
+  gpioa_init_struct.GPIO_Pin = GPIO_Pin_2;
+  gpioa_init_struct.GPIO_Speed = GPIO_Speed_50MHz;
+  gpioa_init_struct.GPIO_Mode = GPIO_Mode_AF_PP;
+  GPIO_Init(GPIOA, &gpioa_init_struct);
+
+  gpioa_init_struct.GPIO_Pin = GPIO_Pin_3;
+  gpioa_init_struct.GPIO_Speed = GPIO_Speed_50MHz;
+  gpioa_init_struct.GPIO_Mode = GPIO_Mode_IN_FLOATING;
+  GPIO_Init(GPIOA, &gpioa_init_struct);
+
+  usart2_init_struct.USART_BaudRate = 9600;
+  usart2_init_struct.USART_WordLength = USART_WordLength_8b;
+  usart2_init_struct.USART_StopBits = USART_StopBits_1;
+  usart2_init_struct.USART_Parity = USART_Parity_No;
+  usart2_init_struct.USART_Mode = USART_Mode_Rx | USART_Mode_Tx;
+  usart2_init_struct.USART_HardwareFlowControl = USART_HardwareFlowControl_None;
+  USART_Init(USART2, &usart2_init_struct);
+  USART_Cmd(USART2, ENABLE);
+  USART_ITConfig(USART2, USART_IT_RXNE, ENABLE);
+
+  NVIC_InitStructure.NVIC_IRQChannel = USART2_IRQn;
+  NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 0x01;
+  NVIC_InitStructure.NVIC_IRQChannelSubPriority = 0x01;
+  NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
+  NVIC_Init(&NVIC_InitStructure);
+
+}
+void USART1_IRQHandler() {
+
+uint16_t word;
+        if(USART_GetITStatus(USART1,USART_IT_RXNE)!=RESET){
+        word = USART_ReceiveData(USART1);
+        USART_SendData(USART2, word);
+        USART_ClearITPendingBit(USART1,USART_IT_RXNE);
+    }
+}
+
+char msg[100];
+uint8_t mych='b';
+
+void USART2_IRQHandler(void) {
+  uint16_t word;
+
+       if(USART_GetITStatus(USART2,USART_IT_RXNE)!=RESET){
+          word =  USART_ReceiveData(USART2);
+          USART_SendData(USART1, word);
+          
+          if(word == 'a'){
+            mych = 'a';
+          }
+          else if(word == 'b'){
+            mych = 'b';
+          }
+          USART_ClearITPendingBit(USART2, USART_IT_RXNE);
+      
+       }
+}
+
+
 
 #include <ws2812b.h>
 
@@ -94,16 +215,28 @@ void ADC1_2_IRQHandler(void) {
 
 RGB_t leds[NUM_LEDS];
 
+
+void Delay(void) {
+int i;
+
+for (i = 0; i < 2000000; i++) {}
+}
+
+int flag_a= 0;
+int flag_b=0;
 int main() {
-    ws2812b_Init();
+   ws2812b_Init();
     SystemInit();
-    set_ENABLE();
+   ws2812b_Init();
+   set_ENABLE();
     set_PC1();
     set_ADC();
     set_NVIC();
-    ADC_start();
-    LCD_Init();
-    LCD_Clear(WHITE);
+   ADC_start();
+   LCD_Init();
+   LCD_Clear(WHITE);   
+    USART1_Init();
+    USART2_Init();
 
     while (1) {
        // while (!ws2812b_IsReady()); // wait
@@ -114,8 +247,26 @@ int main() {
       
         LCD_ShowNum(1, 200, jodo, 4, BLACK, WHITE);
         LCD_ShowString(1, 1, "Wed_team07", BLACK, WHITE);
-            
-       
+          
+        if(mych == 'a'){
+          if(flag_a ==0){
+            LCD_Clear(WHITE);   
+            flag_a =1;
+            flag_b = 0;
+          }
+          LCD_ShowNum(1, 200, jodo, 4, BLACK, WHITE);
+          LCD_ShowString(1, 1, "Wed_team07", BLACK, WHITE);
+        }
+        else if(mych == 'b'){
+          if(flag_b ==0){
+            LCD_Clear(GRAY);
+            flag_b = 1;
+            flag_a = 0;
+          }
+          LCD_ShowNum(1, 200, jodo, 4, WHITE, GRAY);
+          LCD_ShowString(1, 1, "Wed_team07", WHITE, GRAY);
+        }
+
         
         if (jodo >500)
         {
@@ -151,7 +302,6 @@ int main() {
         }
          ws2812b_SendRGB(leds, NUM_LEDS);
     }
-    
     
     
     }
